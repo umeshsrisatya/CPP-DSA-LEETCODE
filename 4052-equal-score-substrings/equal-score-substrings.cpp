bb2d1ea4b3ac3c7875a2adbc88205818b35310b3
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-03-16 15:33:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (73.89%) - LeetSync

--- 4052-equal-score-substrings/equal-score-substrings.cpp
+++ 4052-equal-score-substrings/equal-score-substrings.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool scoreBalance(string s) {
-        vector<int> arr(s.size());
         int i = 0;
         int sum = 0;
         for(char ch: s){
             sum += ch-'a' +1;
-            arr[i] = sum;
-            i++;
         }
-        int totalScore = arr[s.size()-1];
-        for(int i : arr){
-            if((i*2) == (totalScore)){
-                return true;
-            }else if((i*2) >(totalScore)){
-                return false;
-            }
+        int prefixSum = 0;
+        for(char ch: s){
+            prefixSum += ch-'a' +1;
+            if(prefixSum *2 == sum) return true;
+            else if (prefixSum *2 > sum) return false;
         }
         return false;
     }
