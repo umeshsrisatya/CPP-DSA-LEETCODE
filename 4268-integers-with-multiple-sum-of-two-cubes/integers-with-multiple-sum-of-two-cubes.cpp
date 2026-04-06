c219829799e131eddb4985570fe9da76d33a98c2
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-04-06 11:14:42 +0530

    Time: 1087 ms (10.53%) | Memory: 195.3 MB (15.79%) - LeetSync

--- 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.cpp
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> findGoodIntegers(int n) {
         map<int, int> mpp; // to store frequencies of sum;
-
+        //why <= means cbrt(4104) = 16.0104 but int only takes 16 so we need to calculate the a value which cbrt = 0.0104
         int limit = cbrt(n);
-        for(int a = 1; a <= limit; a++){
+        for(int a = 1; a <= limit; a++){ 
             int a3 = a * a * a;
 
             for(int b = a ; b <= limit; b++){
