704a26a6d62223550055cd27a8cea68928920723
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-04-17 12:34:47 +0530

    Time: 207 ms (34.56%) | Memory: 133.9 MB (41.67%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -4,7 +4,6 @@ public:
         int mini = INT_MAX;
         unordered_map<int,int> mpp;
         int n = nums.size();
-        bool flag = false;
         for(int i = 0; i < n; i++){
             string r = to_string(nums[i]);
             reverse(r.begin(),r.end());
@@ -12,12 +11,11 @@ public:
             if(mpp.find(nums[i]) == mpp.end()){
                 mpp[reverse] = i;
             }else{
-                flag = true;
                 mini = min(mini, abs(i - mpp[nums[i]]));
                 mpp[reverse] = i ;
             }
         }
-        if(flag == false) return -1;
+        if(mini == INT_MAX) return -1;
         return mini;
     }
 };
