2b474b02d34d8231542bbe26f094fb3e6edc6c1f
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-05-15 01:14:14 +0530

    Time: 7 ms (18.89%) | Memory: 9.6 MB (74.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {//good thought process im taking a nap txt me in ws // ok
-        map<char, int> mpp;
-        if(s.size() != t.size()){
-            return false;
-        }
-        for(char i:s){
-            mpp[i]++;
-        }
-        for(char i : t){
-            if(mpp.find(i)!=mpp.end()){
-                mpp[i]--;//hey fucker this approach will make the map element second to 0// that's why i asked u is this correct //this is ok. change the code
-            }
-        } 
-        bool flag = true;
-        for(auto i : mpp){
-            if(i.second >0) flag = false;
-        }
-        if(flag == true )return true;
-        return false;        // solve this test case my fucking lady
-        //osey rondu size veru vunnai ey bujji s &t
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
