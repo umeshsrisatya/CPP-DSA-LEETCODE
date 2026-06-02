b8bf493655be16e16c84efee509bb9e398026196
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-06-02 22:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (71.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
-        int s = m + n - 1;
         for (int i = m + n - 1; i >= 0; i--) {
             if (m == 0)
                 n1[i] = n2[--n];
             else if (n == 0)
-                n1[i] = n1[--m];
+                return;
             else if (n1[m - 1] >= n2[n - 1])
                 n1[i] = n1[--m];
             else
